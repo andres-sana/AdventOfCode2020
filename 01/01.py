58d692c56ca45b7ab1a527eb09e6fddeece9df9b
andres-sana <andres.sana95@gmail.com>
2020-12-03 00:07:37 -0300

    update - performance improvement

--- 01/01.py
+++ 01/01.py
@@ -1,8 +1,11 @@
-inp = open('input01.txt').read().split('\n')
+inp = open('InputData.txt').read().split('\n')
 
-for x in range(len(inp)):
-    firstEntry = int(inp[x])
-    for i in range(len(inp)):
-        secondEntry = int(inp[i])
-        if firstEntry + secondEntry == 2020:
-            print(firstEntry * secondEntry)
+def findCombination():
+    for x in range(len(inp)):
+        firstEntry = int(inp[x])
+        for i in range(len(inp)):
+            secondEntry = int(inp[i])
+            if firstEntry + secondEntry == 2020:
+                return firstEntry * secondEntry  
+
+print(findCombination())
--- 01/02.py
+++ 01/02.py
@@ -1,11 +1,13 @@
-inp = open('input01.txt').read().split('\n')
+inp = open('InputData.txt').read().split('\n')
 
-for x in range(len(inp)):
-    firstEntry = int(inp[x])
-    for i in range(len(inp)):
-        secondEntry = int(inp[i])
-        for k in range(len(inp)):
-            thirdEntry = int(inp[k])
-            if firstEntry + secondEntry + thirdEntry == 2020:
-                print(firstEntry * secondEntry * thirdEntry)
-            
+def findCombination():
+    for x in range(len(inp)):
+        firstEntry = int(inp[x])
+        for i in range(len(inp)):
+            secondEntry = int(inp[i])
+            for k in range(len(inp)):
+                thirdEntry = int(inp[k])
+                if firstEntry + secondEntry + thirdEntry == 2020:
+                    return firstEntry * secondEntry * thirdEntry
+
+print(findCombination())
